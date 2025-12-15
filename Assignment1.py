c799aa99bbd92ccc72b3de38fabe9c5bb46b040c
ShaswatRaj01 <shashwatrajsingh654@gmail.com>
2025-12-16 03:26:47 +0530

    programs

---
+++ Assignment1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+p = a+b
+q = a-b
+r = a*b
+s = a/b
+print("Addition: ",p)
+print("Subtraction:",q)
+print("Multiplication:",r)
+print("Division:",s)
